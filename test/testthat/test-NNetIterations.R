5b253c124f11f91869c80466c6f4b16c428fd8e7
gotlr <43156536+gotlr@users.noreply.github.com>
2019-04-09 23:40:37 -0700

    Add files via upload

--- test/testthat/test-NNetIterations.R
+++ test/testthat/test-NNetIterations.R
@@ -6,26 +6,26 @@
 data(SAheart, package = "ElemStatLearn")
 y.vec = as.vector(SAheart[,dim(SAheart)[2]])
 X.mat = SAheart[,-dim(SAheart)[2]]
-is.train = rep(0,length(y.vec))
+is.train = rep(TRUE,length(y.vec))
 fold.vec = sample(rep(1:2), length(y.vec),TRUE)
 is.train[which(fold.vec == 1)] = FALSE
 is.train[which(fold.vec != 1)] = TRUE
 max.iterations = 30L
 step.size = 0.5
 n.hidden.units = 15L
-NNetIterations <- function(X.mat,y.vec,max.iterations,step.size,n.hidden.units,is.train)
+NNetIterations(X.mat,y.vec,max.iterations,step.size,n.hidden.units,is.train)
 ## this will return: X.mat must be a numberic matrix!
 
 
-X.mat = data.matrix(SAheart[,-dim(SAheart)[2]])
-NNetIterations <- function(X.mat,y.vec,max.iterations,step.size,n.hidden.units,is.train)
+X.mat = data.matrix(X.mat)
+NNetIterations(X.mat,y.vec,max.iterations,step.size,n.hidden.units,is.train)
 #return the expected type
 
 
 #example 2 : regression
 data(prostate, package = "ElemStatLearn")
-y.vec = as.vector(prostate[,dim(SAheart)[2]-1])
-X.mat = SAheart[,c(-dim(SAheart)[2],-dim(SAheart)[2]-1)]
+y.vec = as.vector(prostate[,dim(prostate)[2]-1])
+X.mat = prostate[,c(-dim(prostate)[2],-dim(prostate)[2]-1)]
 is.train = rep(0,length(y.vec))
 fold.vec = sample(rep(1:2), length(y.vec),TRUE)
 is.train[which(fold.vec == 1)] = FALSE
@@ -33,10 +33,10 @@ is.train[which(fold.vec != 1)] = TRUE
 max.iterations = 30L
 step.size = 0.5
 n.hidden.units = 15L
-NNetIterations <- function(X.mat,y.vec,max.iterations,step.size,n.hidden.units,is.train)
+NNetIterations(X.mat,y.vec,max.iterations,step.size,n.hidden.units,is.train)
 ## this will return: X.mat must be a numberic matrix!
 
 
-X.mat = data.matrix(SAheart[,-dim(SAheart)[2]])
-NNetIterations <- function(X.mat,y.vec,max.iterations,step.size,n.hidden.units,is.train)
+X.mat = data.matrix(X.mat)
+NNetIterations(X.mat,y.vec,max.iterations,step.size,n.hidden.units,is.train)
 #this will return the expected type
